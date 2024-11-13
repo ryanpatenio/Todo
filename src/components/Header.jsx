b04f1dec6c8659e87e16079fa1225fbfcb765d4e
ryanpatenio <ryanpatenio06@gmail.com>
2024-11-13 20:57:16 +0800

    change Title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                My Diary
+                To Do!
               </Typography>
               <Button color="inherit">Login</Button>
             </Toolbar>
